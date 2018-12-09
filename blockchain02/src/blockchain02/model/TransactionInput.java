1ac493027545f350f1fd12464072ef5b00b36a08
Carlos Martins <guttomarttins@gmail.com>
2018-12-09 11:37:06 -0200

    tranferencia da carteira a para b

---
+++ blockchain02/src/blockchain02/model/TransactionInput.java
@@ -0,0 +1,15 @@
+package blockchain02.model;
+
+/**
+ *
+ * @author Carlos
+ */
+public class TransactionInput {
+
+    public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; //Contains the Unspent transaction output
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+}
